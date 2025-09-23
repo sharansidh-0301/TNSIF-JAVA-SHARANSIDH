6c1256e96af6719a9e38fea45ae2df49f490b18b
SHARANSIDH J R <sharansidh0301@gmail.com>
2025-09-23 14:48:55 +0530

    commit 1

---
+++ tnsif_java_sharansidh/src/basics/of/java/DemoJava.java
@@ -0,0 +1,26 @@
+package basics.of.java;
+import java.util.*;
+
+class human{
+	boolean haveEyes;
+	String color;
+	boolean haveHands;
+	
+	human(boolean e, String c, boolean h){
+		this.color = c;
+		this.haveEyes = e;
+		this.haveHands =h;
+	}
+}
+
+public class DemoJava {
+
+	public static void main(String[] args) {
+		
+		human obj = new human(true,"White",false);
+		System.out.println(obj.color);
+		System.out.println(obj.haveEyes);
+		System.out.println(obj.haveHands);
+	}
+
+}
