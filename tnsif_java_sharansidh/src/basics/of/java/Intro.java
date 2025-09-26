7336d03a92e4c50733e262fcc4fdd5a4750ab432
SHARANSIDH J R <sharansidh0301@gmail.com>
2025-09-26 13:56:39 +0530

    commit 9

---
+++ tnsif_java_sharansidh/src/basics/of/java/Intro.java
@@ -0,0 +1,20 @@
+package basics.of.java;
+
+/*
+ * Java - 1995 
+ * Oak
+ * Created by Sun Microsystems in Island in Indonesia
+ * Now owned by Oracle
+ * Web Application - Whatsapp (hosted in Server)
+ * 
+ * Interview Questions:
+ * 1. What are the Java terminologies?
+ * 		Bytecode, JDK,(JRE) Java Runtime Environment, JVM
+ * 		Bytecode is .class file
+ * 		C/C++ converted into machine code
+ * 		JDk - 
+ * */
+
+public class Intro {
+
+}
